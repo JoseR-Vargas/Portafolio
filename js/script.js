d0806be02878015dc4d700eb45e13c5f91ed8b0d
Jose Vargas <rafael.uruguay2022@gmail.com>
2024-03-23 19:27:12 -0300

    changes

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// prompt
+
+// let respuesta = prompt('De que zona nos contactas')
+
+
+// alert
+// alert('debes aceptar los terminos y condiciones')
+
+// confirm
+// let confirmar = confirm('Debes aceptar los terminos y condiciones')
+
+// concatenacion
+
+// let platoElegido = prompt ('Que quieres comer ?');
+
+// alert('Hoy es un lindo dia' + platoElegido);
+
+// let platoPrincipal = 1000;
+// let Bebida = 300;
+
+// let totalPedido = platoPrincipal + Bebida;
+
+// let nombre = prompt("Ingrese su nombre");
+// let apellido = prompt("Ingrese su apellido");
+
+// let nombreCompleto = nombre + " " + apellido; 
+
+// console.log("Bienvenid@",nombreCompleto);
